40f0faa3e74ff8d15910272ccd814e9f9942e22f
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-26 00:01:06 +0530

    Create  ecs-service.tf

---
+++ terraform/ecs-service.tf
@@ -0,0 +1,21 @@
+resource "aws_ecs_service" "strapi_service" {
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.strapi_cluster.id
+  task_definition = aws_ecs_task_definition.strapi_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.subnet_ids
+    security_groups = [aws_security_group.ecs_service_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.strapi_tg.arn
+    container_name   = "strapi"
+    container_port   = 1337
+  }
+
+  depends_on = [aws_lb_listener.strapi_listener]
+}
