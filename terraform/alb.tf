d1fce6a6d7affc7fd1e1e6aef1d3ed0a25ea33d6
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-26 01:09:17 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,9 +1,34 @@
+resource "aws_security_group" "lb_sg" {
+  name        = "strapi-lb-sg"
+  description = "Allow HTTP access to ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_lb" "strapi_alb" {
   name               = "strapi-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
   subnets            = var.subnet_ids
+
+  enable_deletion_protection = false
+  idle_timeout               = 60
 }
 
 resource "aws_lb_target_group" "strapi_tg" {
@@ -11,6 +36,8 @@ resource "aws_lb_target_group" "strapi_tg" {
   port     = 1337
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+
+  target_type = "ip"
 
   health_check {
     path                = "/"
@@ -31,25 +58,5 @@ resource "aws_lb_listener" "strapi_listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.strapi_tg.arn
-  }
-}
-
-resource "aws_security_group" "lb_sg" {
-  name        = "strapi-lb-sg"
-  description = "Allow HTTP"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
