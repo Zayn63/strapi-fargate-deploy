bd6e2bfde00ed3202886ed0b3c97a3fff7c92469
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-25 23:56:56 +0530

    Create alb.tf

---
+++ terraform/alb.tf
@@ -0,0 +1,55 @@
+resource "aws_lb" "strapi_alb" {
+  name               = "strapi-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = var.subnet_ids
+}
+
+resource "aws_lb_target_group" "strapi_tg" {
+  name     = "strapi-tg"
+  port     = 1337
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "strapi_listener" {
+  load_balancer_arn = aws_lb.strapi_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.strapi_tg.arn
+  }
+}
+
+resource "aws_security_group" "lb_sg" {
+  name        = "strapi-lb-sg"
+  description = "Allow HTTP"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
