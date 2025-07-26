f9d4d08a7b82420d02e250a16acf6f7df79e92c7
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-26 09:27:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,113 @@
+# --- terraform/main.tf ---
 provider "aws" {
-  region = var.aws_region
+  region = "eu-north-1"
 }
 
-data "aws_caller_identity" "current" {}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
 
-resource "aws_ecs_cluster" "strapi_cluster" {
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name = "existing-vpc-ref"
+  manage_default_route_table = false
+  manage_default_security_group = false
+  manage_default_network_acl = false
+  create_vpc = false
+
+  public_subnets = [
+    "subnet-086c3ae98cdde3671",
+    "subnet-0da2d6106d23b40c7"
+  ]
+
+  vpc_id = "vpc-065cf6d69ad9fbf22"
+}
+
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "strapi" {
   name = "strapi-cluster"
 }
 
-resource "aws_ecs_task_definition" "strapi_task" {
+resource "aws_lb" "strapi_alb" {
+  name               = "strapi-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = module.vpc.public_subnets
+  security_groups    = [aws_security_group.strapi_sg.id]
+}
+
+resource "aws_lb_target_group" "strapi_tg" {
+  name     = "strapi-tg"
+  port     = 1337
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener" "strapi_listener" {
+  load_balancer_arn = aws_lb.strapi_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.strapi_tg.arn
+  }
+}
+
+resource "aws_ecs_task_definition" "strapi" {
   family                   = "strapi-task"
   requires_compatibilities = ["FARGATE"]
-  network_mode            = "awsvpc"
-  cpu                     = "512"
-  memory                  = "1024"
-  execution_role_arn      = aws_iam_role.ecs_task_execution_role.arn
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
       name      = "strapi"
-      image     = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repo_name}:latest"
+      image     = "${var.image_repo}:${var.image_tag}"
       essential = true
       portMappings = [
         {
           containerPort = 1337
+          hostPort      = 1337
           protocol      = "tcp"
         }
       ]
@@ -31,17 +115,17 @@ resource "aws_ecs_task_definition" "strapi_task" {
   ])
 }
 
-resource "aws_ecs_service" "strapi_service" {
+resource "aws_ecs_service" "strapi" {
   name            = "strapi-service"
-  cluster         = aws_ecs_cluster.strapi_cluster.id
-  task_definition = aws_ecs_task_definition.strapi_task.arn
-  launch_type     = "FARGATE"
+  cluster         = aws_ecs_cluster.strapi.id
+  task_definition = aws_ecs_task_definition.strapi.arn
   desired_count   = 1
+  launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = var.subnet_ids
+    subnets         = module.vpc.public_subnets
+    security_groups = [aws_security_group.strapi_sg.id]
     assign_public_ip = true
-    security_groups = [aws_security_group.ecs_sg.id]
   }
 
   load_balancer {
@@ -49,6 +133,27 @@ resource "aws_ecs_service" "strapi_service" {
     container_name   = "strapi"
     container_port   = 1337
   }
-
   depends_on = [aws_lb_listener.strapi_listener]
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
