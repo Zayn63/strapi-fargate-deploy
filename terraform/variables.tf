dbb9e82019260397255f7c379caa4013a4b5c1ca
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-26 19:52:16 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,12 @@ variable "image_url" {
   description = "Docker image URL (Docker Hub)"
   type        = string
 }
+variable "image_repo" {
+  description = "Docker image repository"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "Docker image tag"
+  type        = string
+}
