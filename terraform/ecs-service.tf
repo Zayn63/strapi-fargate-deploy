02019419b04c97b0228405eea6e6d802ba9657a4
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-26 18:26:57 +0530

    Update ecs-service.tf

--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -1,55 +1,10 @@
-resource "aws_ecs_cluster" "strapi_cluster" {
-  name = "strapi-cluster"
-}
-
-resource "aws_ecs_task_definition" "strapi_task" {
-  family                   = "strapi-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = var.execution_role_arn
-  container_definitions = jsonencode([{
-    name      = "strapi"
-    image     = "${aws_ecr_repository.strapi_repo.repository_url}:latest"
-    essential = true
-    portMappings = [
-      {
-        containerPort = 1337
-        hostPort      = 1337
-      }
-    ]
-  }])
-}
-
-resource "aws_ecs_service" "strapi_service" {
-  name            = "strapi-service"
-  cluster         = aws_ecs_cluster.strapi_cluster.id
-  task_definition = aws_ecs_task_definition.strapi_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets         = var.subnet_ids
-    security_groups = [aws_security_group.strapi_sg.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.strapi_tg.arn
-    container_name   = "strapi"
-    container_port   = 1337
-  }
-
-  depends_on = [aws_lb_listener.strapi_listener]
-}
-
 resource "aws_security_group" "strapi_sg" {
   name        = "strapi-sg"
-  description = "Allow traffic for Strapi ECS"
-  vpc_id      = var.vpc_id
+  description = "Allow HTTP access"
+  vpc_id      = module.vpc.vpc_id
 
   ingress {
+    description = "HTTP"
     from_port   = 1337
     to_port     = 1337
     protocol    = "tcp"
@@ -62,4 +17,51 @@ resource "aws_security_group" "strapi_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "strapi-sg"
+  }
+}
+
+resource "aws_ecs_cluster" "strapi_cluster" {
+  name = "strapi-cluster"
+}
+
+resource "aws_ecs_task_definition" "strapi_task" {
+  family                   = "strapi-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name      = "strapi"
+    image     = "${var.image_repo}:${var.image_tag}"
+    essential = true
+    portMappings = [{
+      containerPort = 1337
+      hostPort      = 1337
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "strapi_service" {
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.strapi_cluster.id
+  task_definition = aws_ecs_task_definition.strapi_task.arn
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = module.vpc.public_subnets
+    security_groups = [aws_security_group.strapi_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.strapi_tg.arn
+    container_name   = "strapi"
+    container_port   = 1337
+  }
+
+  depends_on = [aws_lb_listener.strapi_listener]
 }
