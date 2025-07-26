e95bd23b25c92821b969c792c95c037758a69e94
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-26 19:13:09 +0530

    Create version.tf

---
+++ terraform/version.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# variables.tf
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "strapi"
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
