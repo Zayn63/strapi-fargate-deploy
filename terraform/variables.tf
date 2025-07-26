ab3cb05eb947d7840b93200226fa356655c45a55
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-26 20:08:46 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,23 @@
-variable "image_url" {
-  description = "Docker image URL (Docker Hub)"
+variable "aws_region" {
+  description = "AWS region"
   type        = string
-}
-variable "image_repo" {
-  description = "Docker image repository"
-  type        = string
+  default     = "eu-north-1"
 }
 
-variable "image_tag" {
-  description = "Docker image tag"
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+  default     = ["subnet-086c3ae98cdde3671", "subnet-0da2d6106d23b40c7"]
+}
+
+variable "docker_image" {
+  description = "Docker image for the Strapi app"
+  type        = string
+  default     = "zayn63/strapi:latest"
 }
