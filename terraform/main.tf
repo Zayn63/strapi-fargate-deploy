98aa11cb5c8bf089c1bdd354eaf74d80517e10ce
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-26 19:21:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,35 +5,32 @@ module "vpc" {
   name = "strapi-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-north-1a", "eu-north-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  azs            = ["eu-north-1a", "eu-north-1b"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
 
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Terraform   = "true"
     Environment = "dev"
+    Terraform   = "true"
   }
 }
 
 resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-sg"
-  description = "Allow HTTP and ECS"
-  vpc_id      = module.vpc.vpc_id
+  name   = "strapi-sg"
+  vpc_id = module.vpc.vpc_id
 
   ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 1337
+    to_port     = 1337
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description = "App Port"
-    from_port   = 1337
-    to_port     = 1337
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -52,24 +49,21 @@ resource "aws_ecs_cluster" "strapi_cluster" {
 
 resource "aws_ecs_task_definition" "strapi_task" {
   family                   = "strapi-task"
-  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
-  container_definitions    = jsonencode([
-    {
-      name  = "strapi"
-      image = var.image_url
-      portMappings = [
-        {
-          containerPort = 1337
-          hostPort      = 1337
-          protocol      = "tcp"
-        }
-      ]
-    }
-  ])
+  network_mode            = "awsvpc"
+  cpu                     = "256"
+  memory                  = "512"
+  execution_role_arn      = aws_iam_role.ecsTaskExecutionRole.arn
+
+  container_definitions = jsonencode([{
+    name      = "strapi"
+    image     = var.image_url
+    essential = true
+    portMappings = [{
+      containerPort = 1337
+      protocol      = "tcp"
+    }]
+  }])
 }
 
 resource "aws_ecs_service" "strapi_service" {
@@ -80,31 +74,28 @@ resource "aws_ecs_service" "strapi_service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = var.subnet_ids
-    security_groups = [aws_security_group.strapi_sg.id]
+    subnets          = module.vpc.public_subnets
+    security_groups  = [aws_security_group.strapi_sg.id]
     assign_public_ip = true
   }
-  depends_on = [aws_ecs_task_definition.strapi_task]
 }
 
-resource "aws_iam_role" "ecs_task_execution" {
+resource "aws_iam_role" "ecsTaskExecutionRole" {
   name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
       }
-    ]
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  role       = aws_iam_role.ecs_task_execution.name
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
