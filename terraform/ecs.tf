147cbbe2421a2886b0956e0477e48662cd4a7e52
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-28 17:32:29 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -33,37 +33,23 @@ resource "aws_ecs_task_definition" "strapi" {
   container_definitions = jsonencode([
     {
       name      = "strapi"
-      image     = var.strapi_image
+      image     = "zayn63/strapi:latest"
       essential = true
-      portMappings = [{
-        containerPort = 1337
-        hostPort      = 1337
-        protocol      = "tcp"
-      }]
-      logConfiguration {
+      portMappings = [
+        {
+          containerPort = 1337
+          hostPort      = 1337
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
         logDriver = "awslogs"
-
         options = {
-          awslogs-group         = aws_cloudwatch_log_group.strapi_logs.name
-          awslogs-region        = var.aws_region
-          awslogs-stream-prefix = "strapi"
-       }
-     }
-
+          awslogs-group         = "/ecs/strapi"
+          awslogs-region        = "eu-north-1"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
     }
   ])
-}
-
-resource "aws_ecs_service" "strapi" {
-  name            = "strapi-service"
-  cluster         = aws_ecs_cluster.strapi.id
-  task_definition = aws_ecs_task_definition.strapi.arn
-  launch_type     = "FARGATE"
-  desired_count   = 1
-
-  network_configuration {
-    subnets         = var.subnet_ids
-    security_groups = [aws_security_group.strapi_sg.id]
-    assign_public_ip = true
-  }
 }
