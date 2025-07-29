4c06219726f84b8455d3fbc29ca6b1bdf00ca953
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-29 21:16:06 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
-output "strapi_url" {
-  value = aws_lb.zstrapi_alb.dns_name
+output "alb_dns" {
+  value = data.aws_lb.strapi_alb.dns_name
 }
 
 output "ecs_cluster_name" {
-  value = aws_ecs_cluster.zstrapi.name
+  value = aws_ecs_cluster.strapi_cluster.name
 }
 
 output "log_group_name" {
-  value = aws_cloudwatch_log_group.zstrapi_logs.name
+  value = data.aws_cloudwatch_log_group.strapi_logs.name
 }
