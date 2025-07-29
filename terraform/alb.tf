c4ae387de0c6223ab3a099670c18f3ca1662c66e
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-29 14:16:12 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,25 +1,25 @@
 resource "aws_lb" "strapi_alb" {
   name               = "strapi-alb"
-  internal           = false
   load_balancer_type = "application"
   security_groups    = [var.security_group_id]
   subnets            = var.subnet_ids
 }
 
 resource "aws_lb_target_group" "strapi_tg" {
-  name        = "strapi-tg"
-  port        = 1337
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
+  name     = "strapi-tg"
+  port     = 1337
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
   target_type = "ip"
 
   health_check {
     path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
-    matcher             = "200"
   }
 }
 
