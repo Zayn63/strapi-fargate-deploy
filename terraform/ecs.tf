b6d8aa4bb3e7b1ca903d56b5dff8efefb841437f
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-29 21:40:57 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,7 +1,29 @@
-resource "aws_ecs_cluster" "strapi_cluster" {
-  name = "strapi-cluster"
+# Reference existing ECS Cluster
+data "aws_ecs_cluster" "strapi_cluster" {
+  cluster_name = "strapi-cluster"
 }
 
+# Reference existing ALB
+data "aws_lb" "strapi_alb" {
+  name = "strapi-alb"
+}
+
+# Reference existing Target Group
+data "aws_lb_target_group" "strapi_tg" {
+  name = "strapi-tg"
+}
+
+# Reference existing IAM Role
+data "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+}
+
+# Reference existing CloudWatch log group
+data "aws_cloudwatch_log_group" "strapi_logs" {
+  name = "/ecs/strapi"
+}
+
+# ECS Task Definition
 resource "aws_ecs_task_definition" "strapi_task" {
   family                   = "strapi-task"
   requires_compatibilities = ["FARGATE"]
@@ -10,29 +32,28 @@ resource "aws_ecs_task_definition" "strapi_task" {
   memory                   = "1024"
   execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
 
-  container_definitions = jsonencode([
-    {
-      name  = "strapi"
-      image = var.image_url
-      portMappings = [{
-        containerPort = 1337
-        hostPort      = 1337
-      }],
-      logConfiguration = {
-        logDriver = "awslogs",
-        options = {
-          awslogs-group         = data.aws_cloudwatch_log_group.strapi_logs.name,
-          awslogs-region        = var.aws_region,
-          awslogs-stream-prefix = "strapi"
-        }
+  container_definitions = jsonencode([{
+    name  = "strapi"
+    image = var.image_url
+    portMappings = [{
+      containerPort = 1337
+      hostPort      = 1337
+    }],
+    logConfiguration = {
+      logDriver = "awslogs",
+      options = {
+        awslogs-group         = data.aws_cloudwatch_log_group.strapi_logs.name,
+        awslogs-region        = var.aws_region,
+        awslogs-stream-prefix = "strapi"
       }
     }
-  ])
+  }])
 }
 
+# ECS Service
 resource "aws_ecs_service" "strapi_service" {
   name            = "strapi-service"
-  cluster         = aws_ecs_cluster.strapi_cluster.id
+  cluster         = data.aws_ecs_cluster.strapi_cluster.id
   task_definition = aws_ecs_task_definition.strapi_task.arn
   launch_type     = "FARGATE"
   desired_count   = 1
