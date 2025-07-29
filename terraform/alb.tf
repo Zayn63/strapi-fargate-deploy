ab46467a78d425f8d87834dfd8c5245c889375bb
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-29 16:54:41 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,13 +1,36 @@
-data "aws_lb" "strapi_alb" {
-  name = "strapi-alb"
+
+resource "aws_lb" "zstrapi_alb" {
+  name               = "zstrapi-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.security_group_id]
+  subnets            = var.subnet_ids
 }
 
-data "aws_lb_target_group" "strapi_tg" {
-  name = "strapi-tg"
+resource "aws_lb_target_group" "zstrapi_tg" {
+  name     = "zstrapi-tg"
+  port     = 1337
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "ip"
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 
-data "aws_lb_listener" "http" {
-  load_balancer_arn = data.aws_lb.strapi_alb.arn
+resource "aws_lb_listener" "zstrapi_listener" {
+  load_balancer_arn = aws_lb.zstrapi_alb.arn
   port              = 80
-}
+  protocol          = "HTTP"
 
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.zstrapi_tg.arn
+  }
+}
