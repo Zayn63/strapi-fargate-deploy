f57969776303e77316d2be3cc149fff93bec6142
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-29 14:14:59 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,26 +1,16 @@
-variable "aws_region" {
-  default = "eu-north-1"
-}
-
 variable "vpc_id" {
   default = "vpc-065cf6d69ad9fbf22"
 }
 
 variable "subnet_ids" {
-  description = "List of subnet IDs for ECS tasks"
-  type        = list(string)
+  type    = list(string)
+  default = ["subnet-086c3ae98cdde3671", "subnet-0da2d6106d23b40c7"]
 }
 
 variable "security_group_id" {
-  description = "Security Group ID for ECS tasks"
-  type        = string
+  default = "sg-0449336e4644cdbb3"
 }
 
-
-variable "strapi_image" {
+variable "docker_image" {
   default = "zayn63/strapi:latest"
-}
-
-variable "key_name" {
-  default = "zayn-key"
 }
