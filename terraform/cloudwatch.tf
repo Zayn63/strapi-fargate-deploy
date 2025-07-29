b4b823f2b5ce84805b17fdcac778342fe1145fb9
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-29 15:55:37 +0530

    Update cloudwatch.tf

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,4 +1,3 @@
-resource "aws_cloudwatch_log_group" "strapi_logs" {
-  name              = "/ecs/strapi"
-  retention_in_days = 7
+data "aws_cloudwatch_log_group" "strapi_logs" {
+  name = "/ecs/strapi"
 }
