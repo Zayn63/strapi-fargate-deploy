6ed570cd1d89f696fcf721ed0d2b6fc8cb5460ff
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-29 14:24:41 +0530

    Create cloudwatch-metrics.tf

---
+++ terraform/cloudwatch-metrics.tf
@@ -0,0 +1,35 @@
+resource "aws_cloudwatch_metric_alarm" "high_cpu" {
+  alarm_name          = "strapi-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 70
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.strapi_cluster.name
+    ServiceName = aws_ecs_service.strapi_service.name
+  }
+
+  alarm_description = "Triggered when ECS CPU usage exceeds 70%"
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_memory" {
+  alarm_name          = "strapi-high-memory"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "MemoryUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 80
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.strapi_cluster.name
+    ServiceName = aws_ecs_service.strapi_service.name
+  }
+
+  alarm_description = "Triggered when ECS memory usage exceeds 80%"
+}
