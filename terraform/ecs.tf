4cd69a0289713f36e36a7876501ac297f5818956
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-29 14:52:07 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -28,7 +28,8 @@ resource "aws_ecs_task_definition" "strapi_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
   memory                   = "1024"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+}
 
   container_definitions = jsonencode([
     {
