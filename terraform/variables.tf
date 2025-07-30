62aeacb6ed42a383428ca5af41bd68b2eea3e6f8
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-30 10:23:32 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,4 @@
-variable "vpc_id" {
-  default = "vpc-065cf6d69ad9fbf22"
-}
-
-variable "subnet_ids" {
-  type    = list(string)
-  default = ["subnet-086c3ae98cdde3671", "subnet-0da2d6106d23b40c7"]
-}
-
-variable "security_group_id" {
-  default = "sg-0449336e4644cdbb3"
-}
-
-variable "docker_image" {
-  default = "zayn63/strapi:latest"
+variable "ecr_image_url" {
+  description = "Full URL of the Docker image in ECR to be used in the ECS task definition"
+  type        = string
 }
